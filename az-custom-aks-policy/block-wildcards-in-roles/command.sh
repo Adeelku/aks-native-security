6276a18740b5745dd8f74b536725510655df487a
Adil Touati <adil.touati@gmail.com>
2022-11-13 16:02:00 -0500

    cleanup

--- az-custom-aks-policy/block-wildcards-in-roles/command.sh
+++
@@ -1,4 +0,0 @@
-az policy definition create --name 'custom-kubernetes-restrict-wildcards-roles-and-clusterrRoles' \
- --display-name 'Custom Kubernetes - Restrict Wildcards in Verbs and Resources for Roles and ClusterRoles' \
- --description 'Custom Kubernetes - Restrict Wildcards in Verbs and Resources for Roles and ClusterRoles' \
- --rules azpolicy.json  --mode All
